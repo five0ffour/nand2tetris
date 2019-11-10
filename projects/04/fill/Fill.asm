b3a1f064c98fb1f4c6a5fb2aac8294864966bc03
Mike Galarneau <mbgalarneau@gmail.com>
2019-11-10 12:03:59 -0500

    protoype screen paint logic, no keyboard logic, word smithed mult for clarity

--- projects/04/fill/Fill.asm
+++ projects/04/fill/Fill.asm
@@ -3,6 +3,10 @@
 // by Nisan and Schocken, MIT Press.
 // File name: projects/04/Fill.asm
 
+// Solution
+//  Author: Michael Galarneau
+//  Date:  November 9, 2019
+
 // Runs an infinite loop that listens to the keyboard input.
 // When a key is pressed (any key), the program blackens the screen,
 // i.e. writes "black" in every pixel;
@@ -11,4 +15,90 @@
 // "white" in every pixel;
 // the screen should remain fully clear as long as no key is pressed.
 
-// Put your code here.
+// pseudo-code of higher order language
+//
+// BLACK = 1
+// WHITE = 0
+// SCREEN = 16384
+// KEY = 24576
+// SCREENSIZE = 8192
+// loc = 0
+// while (true) {
+//     keypressed = KEY[0]
+// 
+//     if (keypressed != 0)
+//        brush = BLACK
+//     else 
+//        brush = WHITE
+// 
+//     SCREEN[loc] = brush
+//     loc = loc + 1
+// 
+//     if loc >= SCREENSIZE
+//        loc = 0
+// 
+// }  /* infinite loop */
+
+
+    // set up constants
+    @BLACK          // black pixel (for when key is pressed)
+    M=-1
+    @WHITE          // white pixel (clear screen)
+    M=0
+
+    @16384          // Set Screen RAM address
+    D=A
+    @SCREENPORT
+    M=D
+
+    @24576          // Set Key RAM address
+    D=A
+    @KEY
+    M=D
+
+    @8192           // Set screensize (in words)
+    D=A
+    @SCREENSIZE
+    M=D
+
+    @SCREENPORT         // set screen location index beginning of screen
+    D=M
+    @loc            
+    M=D
+
+    @keypressed     // initialized as key not pressed (keypressed=0)
+    M=0
+
+    @WHITE          // initalize brush to white, clears screen
+    D=M
+    @brush
+    M=D
+
+(LOOP)
+    // Get the input key
+
+    // if pressed, set brush to black
+    @BLACK
+    D=M
+    @brush
+    M=D
+
+    // if not pressed, set brush to white
+//    @WHITE
+//    D=M
+//    @brush
+//    M=D
+
+(PAINT)
+    @brush      // load the brush setting (black or white)
+    D=M        
+
+    @loc        // write 16 bits out to the screen
+    A=M
+    M=D
+
+    @loc        // increment screen location by one word (16 bits)
+    M=M+1
+
+    @LOOP
+    0;JMP       // Goto LOOP (infinite loop)
--- projects/04/mult/mult.asm
+++ projects/04/mult/mult.asm
@@ -10,16 +10,20 @@
 // Multiplies R0 and R1 and stores the result in R2.
 // (R0, R1, R2 refer to RAM[0], RAM[1], and RAM[2], respectively.)
 
-// Logic:
+// Logical:
+// sum = operand1 * operand2
 // R2 = R0 * R1
+//
+// Higher order pseudo-code:
 // for (i=0, R2=0; (i < R1); i++) {
 //   R2 = R2 + R0   
-//  }
+// }
 
-// Note:  The operands R0 and R1 need to be poked into memory manually
+// Note:  The input operands R0 and R1 need to be poked into 
+//        memory manually before program executes
 //    R0 = operand 1
 //    R1 = operand 2
-//    R2 = value of R0 * R1 
+//    R2 = computed value of R0 * R1 
 
     @i          // initialize the loop counter
     M=0         // i = 0
